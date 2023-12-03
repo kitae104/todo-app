ad2dd1c728fedfaf283f25d7158727764524f95b
Kitae's Github <aqua0405@gmail.com>
2023-12-04 02:27:13 +0900

    Create FooterComponent.jsx

---
+++ src/components/todo/FooterComponent.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { AuthContext } from './security/AuthContext';
+
+function FooterComponent() {
+
+  const authContext = useContext(AuthContext);
+  //console.log(`Footer component - ${authContext.number}`);
+
+  return(
+    <footer className="footer">
+      <div className="container">
+        <span className="text-muted">All Rights Reserved 2023 @kimkitae</span>       
+      </div>
+    </footer>
+  );
+}
+
+export default FooterComponent;
