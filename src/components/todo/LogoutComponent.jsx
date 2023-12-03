0d3e21a9192d7bc0a6bbbe8fff2a22f28ab7a96b
Kitae's Github <aqua0405@gmail.com>
2023-12-04 02:27:41 +0900

    Create LogoutComponent.jsx

---
+++ src/components/todo/LogoutComponent.jsx
@@ -0,0 +1,12 @@
+function LogoutComponent() {
+  return(
+    <div className="LogoutComponent">
+      <h1>로그 아웃 페이지</h1>
+      <div>
+        로그 아웃 되었습니다. 안녕히 가세요.
+      </div>
+    </div>
+  );
+}
+
+export default LogoutComponent;
