a575e8b88abd24ee2e7ecffc7fbdc4765d6123a1
Kitae's Github <aqua0405@gmail.com>
2023-12-05 17:36:59 +0900

    Update TodoApiService.js

--- src/components/todo/api/TodoApiService.js
+++ src/components/todo/api/TodoApiService.js
@@ -2,11 +2,7 @@ import axios from 'axios'
 
 const apiClient = axios.create(
   {
-    baseURL: 'http://localhost',
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    }
+    baseURL: 'http://localhost',    
   }
 );
 
