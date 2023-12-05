9ec057d7ab16aada518ae7e06d7d68e186a62336
Kitae's Github <aqua0405@gmail.com>
2023-12-05 11:25:19 +0900

    Update TodoApiService.js

--- src/components/todo/api/TodoApiService.js
+++ src/components/todo/api/TodoApiService.js
@@ -10,5 +10,10 @@ const apiClient = axios.create(
   }
 );
 
-export const retriveAllTodosForUsername = 
+// 해당 유저 이름에 해당하는 모든 todo를 가져오기
+export const retriveAllTodoForUsernameApi = 
         (username) => apiClient.get(`/users/${username}/todos`);
+
+// 해당 유저 이름에 해당하는 todo id에 해당하는 todo를 삭제하기
+export const deleteTodoApi = 
+        (username, id) => apiClient.delete(`/users/${username}/todos/${id}`);
