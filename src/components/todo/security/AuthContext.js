d52ea97ca434c72ae0c6adcada1e24e6fa9c92bc
Kitae's Github <aqua0405@gmail.com>
2023-12-11 11:45:27 +0900

    Update AuthContext.js

--- src/components/todo/security/AuthContext.js
+++ src/components/todo/security/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState, useContext } from 'react';
-import { executeBasicAuthenticationService } from './../api/HelloWorldApiService';
+import { executeJwtAuthenticationService } from './../api/AuthnticationApiService';
 import { apiClient } from './../api/ApiClient';
 
 // 1. 컨텍스트 생성 
@@ -17,20 +17,51 @@ export default function AuthProvider({children}) {
 
   const [token, setToken] = useState(null);
 
+  // async function login(username, password) {
+  //   const baToken = 'Basic ' + window.btoa(username + ":" + password);
+    
+  //   try{
+  //     const response = await executeBasicAuthenticationService(baToken);
+  //     if(response.status === 200) {
+  //       setAuthenticated(true);       // 인증 여부 false 설정 
+  //       setUsername(username);        // 사용자 이름
+  //       setToken(baToken);            // 토큰 설정 
+
+  //       apiClient.interceptors.request.use(
+  //         (config) =>{
+  //           console.log('intercepting');
+  //           config.headers.Authorization=baToken;
+  //           return config;
+  //         }
+  //       );
+
+  //       return true;                  // 인증 성공
+  //     } else {
+  //       logout();                     // 로그 아웃 수행 
+  //       return false;                 // 인증 실패
+  //     }
+  //   } catch(error) {
+  //     logout();                     // 로그 아웃 수행 
+  //     return false;                 // 인증 실패
+  //   }
+
+  // }
+
   async function login(username, password) {
-    const baToken = 'Basic ' + window.btoa(username + ":" + password);
     
     try{
-      const response = await executeBasicAuthenticationService(baToken);
+      const response = await executeJwtAuthenticationService(username, password);
+
       if(response.status === 200) {
+        const jwtToken = 'Bearer ' + response.data.token;     // 토큰 생성
         setAuthenticated(true);       // 인증 여부 false 설정 
         setUsername(username);        // 사용자 이름
-        setToken(baToken);            // 토큰 설정 
+        setToken(jwtToken);            // 토큰 설정 
 
         apiClient.interceptors.request.use(
           (config) =>{
             console.log('intercepting');
-            config.headers.Authorization=baToken;
+            config.headers.Authorization=jwtToken;
             return config;
           }
         );
